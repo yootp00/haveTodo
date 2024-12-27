05f00ef9d8b3a6ce7dadd1375c563b88f9a4777c
baeyj <baeyj00@gmail.com>
2024-12-27 23:16:55 +0900

    초기 설정

---
+++ src/main/java/com/example/haveTodo/HaveTodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.haveTodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaveTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaveTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/haveTodo/HaveTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.haveTodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaveTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
